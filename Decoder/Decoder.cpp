c946f222ebe4454f5e9b50c64147b2f2ecf3ab73
foxunderground0 <umerirfan1205@gmail.com>
2023-07-12 21:42:25 +0000

    Component info added

--- Decoder/Decoder.cpp
+++ Decoder/Decoder.cpp
@@ -18,11 +18,24 @@ vector<vector<uint8_t>> quantization_tables;
 vector<unordered_map<uint16_t, tuple<uint8_t, uint8_t>>> huffman_tables;
 int8_t bit_index = 7;
 uint64_t byte_index = 0;
-struct img_header{
-	uint16_t height;
-	uint16_t width;
-}
-uint8_t get_next_bit_from_stream(std::array<uint8_t, IMG_data_len>& arr) {
+
+struct components_info{
+	uint8_t component_id = 0;
+	uint8_t	component_height_subsampling = 0;
+	uint8_t component_width_subsampling = 0;
+	uint8_t component_destination = 0;
+	
+};
+
+struct img_header_info{
+	uint16_t height = 0;
+	uint16_t width = 0;
+	uint8_t number_of_components = 0;
+	vector<*components_info> components_info_vector; //component number, width subsampling, height subsampling, destination
+};
+
+
+uint8_t get_next_bit_from_stream(std::array<uint8_t, img_data_len>& arr) {
 	uint8_t bit = 0;
 	bit = arr.at(byte_index);
 	bit = bit >> bit_index;
@@ -35,19 +48,18 @@ uint8_t get_next_bit_from_stream(std::array<uint8_t, IMG_data_len>& arr) {
 	return bit;
 }
 
-bool check_if_valid_img(std::array<uint8_t, IMG_data_len>& arr) {
-	if (arr.at(0) == 0xff && arr.at(1) == 0xd8) {
+bool check_if_valid_img(std::array<uint8_t, img_data_len>& arr) {
+	if (((arr.at(0) << 8) | arr.at(1)) == ((0xff<<8) | SOI )) {
 		print("JPEG header found")
-			if (arr.at(arr.size() - 2) == 0xff && arr.at(arr.size() - 1) == 0xd9) {
+			if (((arr.at(arr.size() - 2) << 8) | arr.at(arr.size() - 1)) == ((0xff<<8) | EOI )) {
 				print("JPEG end found")
 					return 0;
 			}
-		return 2;
 	}
 	return 1;
 }
 
-uint8_t count_instances(std::array<uint8_t, IMG_data_len>& arr, uint16_t data) {
+uint8_t count_instances(std::array<uint8_t, img_data_len>& arr, uint16_t data) {
 	uint8_t instance_counter = 0;
 	for (unsigned int i = 0; i < arr.size(); i++) {
 		if (arr.at(i) == (data >> 8) && arr.at(i + 1) == (data & 0xff)) {
@@ -57,7 +69,7 @@ uint8_t count_instances(std::array<uint8_t, IMG_data_len>& arr, uint16_t data) {
 	return instance_counter;
 }
 
-tuple<uint8_t, uint16_t, uint8_t> find_quantization_table_position_info(std::array<unsigned char, IMG_data_len>& arr, unsigned int instance = 0) {
+tuple<uint8_t, uint16_t, uint8_t> find_quantization_table_position_info(std::array<unsigned char, img_data_len>& arr, unsigned int instance = 0) {
 	unsigned int instance_counter = 0;
 	for (unsigned int i = 0; i < arr.size(); i++) {
 		if (arr.at(i) == (0xffdb >> 8) && arr.at(i + 1) == (0xffdb & 0xff)) {
@@ -70,7 +82,7 @@ tuple<uint8_t, uint16_t, uint8_t> find_quantization_table_position_info(std::arr
 	return { -1, -1 , -1 };
 }
 
-tuple<uint64_t, uint16_t, uint8_t, uint8_t> find_huffman_table_position_info(std::array<unsigned char, IMG_data_len>& arr, unsigned int instance = 0) {
+tuple<uint64_t, uint16_t, uint8_t, uint8_t> find_huffman_table_position_info(std::array<unsigned char, img_data_len>& arr, unsigned int instance = 0) {
 	unsigned int instance_counter = 0;
 	for (unsigned int i = 0; i < arr.size(); i++) {
 		if (arr.at(i) == (0xffc4 >> 8) && arr.at(i + 1) == (0xffc4 & 0xff)) {
@@ -83,16 +95,30 @@ tuple<uint64_t, uint16_t, uint8_t, uint8_t> find_huffman_table_position_info(std
 	return { -1, -1 , -1, -1 };
 }
 
-tuple<uint64_t, uint16_t, uint16_t> find_start_of_frame_info(std::array<unsigned char, IMG_data_len>& arr) {
-	for (unsigned int i = 0; i < arr.size(); i++) {
-		if (arr.at(i) == (0xffc0 >> 8) && arr.at(i + 1) == (0xffc0 & 0xff) && arr.at(i + 4) == (0x08)) {
-			return { i + 2, arr.at(i + 5) << 8 | arr.at(i + 6), arr.at(i + 7) << 8 | arr.at(i + 8) }; // index immedeately after tag, Height, Width
+img_header_info* find_start_of_frame_info(std::array<unsigned char, img_data_len>& arr) {
+	for (unsigned int i = 0; i < arr.size(); i++) { // Parse file
+		if (((arr.at(i) << 8) | arr.at(i + 1)) == ((0xff<<8) | SOF0 ) && arr.at(i + 4) == (0x08)) { // Check if header is found and the image is of 8 bit per channel color depth
+			img_header_info* pointer = new img_header_info;
+			pointer->height = arr.at(i + 5) << 8 | arr.at(i + 6);
+			pointer->width = arr.at(i + 7) << 8 | arr.at(i + 8);
+			pointer->number_of_components = arr.at(i + 9);
+			i = i+10; // Set the index to the start of components
+			for(uint8_t j = 0; j< number_of_components; j++){
+				components_info* component_pointer = new components_info;
+				component_pointer->component_id = arr.at(i); // TODO
+				component_pointer->component_height_subsampling = arr.at(i);
+				component_pointer->component_width_subsampling = arr.at(i);
+				component_pointer->component_destination = arr.at(i);
+				
+			}
+			//, arr.at(i + 7) << 8 | arr.at(i + 8);
+			return pointer;
 		}
 	}
-	return { -1, -1 , -1 };
+	return nullptr;
 }
 
-uint64_t get_start_of_byte_stream(std::array<unsigned char, IMG_data_len>& arr) {
+uint64_t get_start_of_byte_stream(std::array<unsigned char, img_data_len>& arr) {
 	for (unsigned int i = 0; i < arr.size(); i++) {
 		if (arr.at(i) == (0xffda >> 8) && arr.at(i + 1) == (0xffda & 0xff)) {
 			return  i + 2 + (arr.at(i + 2) << 8 | arr.at(i + 3)); // index immedeately after tag, Height, Width
@@ -111,16 +137,28 @@ uint8_t get_symbol_from_huffman_map(unordered_map<uint16_t, tuple<uint8_t, uint8
 int main(void) {
 
 	print("Entered");
-	print("Size: " << IMG_data.size() << " bytes");
+	print("Size: " << img_data.size() << " bytes");
 	print("Parsing");
 
-	if (check_if_valid_img(IMG_data)) {
+	if (check_if_valid_img(img_data)) {
 		print("Not a valid JPEG immage");
 		return 1;
 	}
+	
+	//Get Frame Height, Width, and info on huffman tables;
+	auto frame_info = find_start_of_frame_info(img_data);
 
+	if(frame_info == nullptr){
+		print("Error making frame_info struct");
+		return 1;
+	}
+
+	print("Frame Height: " << static_cast<int>(frame_info->height));
+	print("Frame Width: " << static_cast<int>(frame_info->width));
+
+	/*
 	//Get Quantization Tables
-	uint8_t num_of_quantization_tables = count_instances(IMG_data, 0xffdb);
+	uint8_t num_of_quantization_tables = count_instances(img_data, 0xffdb);
 	print("Number of Quantization tables found: " << static_cast<int>(num_of_quantization_tables));
 
 	if (num_of_quantization_tables <= 0) {
@@ -129,14 +167,14 @@ int main(void) {
 	} else {
 		for (uint8_t i = 0; i < num_of_quantization_tables; i++) {
 			print("Quantization table number: " << static_cast<int>(i));
-			auto data = find_quantization_table_position_info(IMG_data, i);
+			auto data = find_quantization_table_position_info(img_data, i);
 			print("Index: " << static_cast<int>(get<0>(data)));
 			print("Length: " << get<1>(data));
 			print("Destination: " << static_cast<int>(get<2>(data)));
 
 			vector<uint8_t> quantization_table_values;
 			for (uint16_t i = get<0>(data) + 3; i < get<0>(data) + get<1>(data); i++) { // Loop over the quantization tables contents
-				quantization_table_values.push_back(IMG_data.at(i));
+				quantization_table_values.push_back(img_data.at(i));
 			}
 			quantization_tables.push_back(quantization_table_values);
 		}
@@ -156,7 +194,7 @@ int main(void) {
 	}
 
 	//Get Huffman Tables
-	uint8_t num_of_huffman_tables = count_instances(IMG_data, 0xffc4);
+	uint8_t num_of_huffman_tables = count_instances(img_data, 0xffc4);
 	print("Number of Huffman tables found: " << static_cast<int>(num_of_huffman_tables));
 
 	if (num_of_quantization_tables <= 0) {
@@ -165,7 +203,7 @@ int main(void) {
 	} else {
 		for (uint8_t i = 0; i < num_of_huffman_tables; i++) {
 			print("Huffman table number: " << static_cast<int>(i));
-			auto data = find_huffman_table_position_info(IMG_data, i);
+			auto data = find_huffman_table_position_info(img_data, i);
 			print("Index: " << static_cast<int>(get<0>(data)));
 			print("Length: " << get<1>(data));
 			print("Class: " << static_cast<int>(get<2>(data)));
@@ -179,12 +217,12 @@ int main(void) {
 
 			for (uint16_t i = get<0>(data) + 3; i < get<0>(data) + get<1>(data); i++) { // Loop over the Huffman tables contents
 				if ((i - (get<0>(data) + 3)) < 16) {
-					cout << static_cast<int>(IMG_data.at(i)) << " ";
-					elements_count += static_cast<int>(IMG_data.at(i));
-					length.push_back(IMG_data.at(i));
+					cout << static_cast<int>(img_data.at(i)) << " ";
+					elements_count += static_cast<int>(img_data.at(i));
+					length.push_back(img_data.at(i));
 				} else {
-					cout << static_cast<int>(IMG_data.at(i)) << " ";
-					elements.push_back(IMG_data.at(i));
+					cout << static_cast<int>(img_data.at(i)) << " ";
+					elements.push_back(img_data.at(i));
 				}
 			}
 
@@ -228,19 +266,19 @@ int main(void) {
 	}
 
 	//Get Frame Height and Width
-	auto frame_info = find_start_of_frame_info(IMG_data);
+	auto frame_info = find_start_of_frame_info(img_data);
 	print("Frame Height: " << static_cast<int>(get<1>(frame_info)));
 	print("Frame Width: " << static_cast<int>(get<2>(frame_info)));
 
 
-	uint64_t start_of_byte_stream_address = get_start_of_byte_stream(IMG_data);
+	uint64_t start_of_byte_stream_address = get_start_of_byte_stream(img_data);
 	print(start_of_byte_stream_address);
 	byte_index = start_of_byte_stream_address;
 
 	uint16_t stream_temporary_container = 0;
 	uint8_t length = 0;
 	for (uint8_t i = 0; i < 8; i++) {
-		stream_temporary_container = (stream_temporary_container << 1) | (get_next_bit_from_stream(IMG_data));
+		stream_temporary_container = (stream_temporary_container << 1) | (get_next_bit_from_stream(img_data));
 		length = get_symbol_from_huffman_map(huffman_tables[0], stream_temporary_container);
 		print(bitset<16>(stream_temporary_container) << " " << (int)length);
 		if (length != 0xff) {
@@ -254,7 +292,7 @@ int main(void) {
 
 	stream_temporary_container = 0;
 	for (uint8_t i = 0; i < length; i++) {
-		stream_temporary_container = (stream_temporary_container << 1) | (get_next_bit_from_stream(IMG_data));
+		stream_temporary_container = (stream_temporary_container << 1) | (get_next_bit_from_stream(img_data));
 	}
 
 	int8_t coefficent = (stream_temporary_container);
@@ -269,7 +307,7 @@ int main(void) {
 	for (uint i = 1; i < 64; ++i) {
 		length = 0;
 		for (uint8_t i = 0; i < 8; i++) {
-			stream_temporary_container = (stream_temporary_container << 1) | (get_next_bit_from_stream(IMG_data));
+			stream_temporary_container = (stream_temporary_container << 1) | (get_next_bit_from_stream(img_data));
 			length = get_symbol_from_huffman_map(huffman_tables[1], stream_temporary_container);
 			print(bitset<16>(stream_temporary_container) << " " << (int)length);
 			if (length != 0xff) {
@@ -304,7 +342,7 @@ int main(void) {
 		}
 
 		for (uint8_t i = 0; i < 8; i++) {
-			stream_temporary_container = (stream_temporary_container << 1) | (get_next_bit_from_stream(IMG_data));
+			stream_temporary_container = (stream_temporary_container << 1) | (get_next_bit_from_stream(img_data));
 			length = get_symbol_from_huffman_map(huffman_tables[1], stream_temporary_container);
 			print(bitset<16>(stream_temporary_container) << " " << (int)length);
 			if (length != 0xff) {
@@ -321,7 +359,7 @@ int main(void) {
 		}
 		print((uint)zigZagMap[i] << " " << length);
 	}
-
+	*/
 	return 0;
 }
 
--- Decoder/jpeg_data.h
+++ Decoder/jpeg_data.h
@@ -5,7 +5,7 @@
 #ifndef IMG_DATA_HEADER
 #define IMG_DATA_HEADER
 
-const int IMG_data_len = 395;// 614236
+const int img_data_len = 395;// 614236
 
 const uint8_t zigZagMap[] = {
     0,   1,  8, 16,  9,  2,  3, 10,
@@ -18,7 +18,93 @@ const uint8_t zigZagMap[] = {
     53, 60, 61, 54, 47, 55, 62, 63
 };
 
+// Start of Frame markers, non-differential, Huffman coding
+const uint8_t SOF0 = 0xC0; // Baseline DCT
+const uint8_t SOF1 = 0xC1; // Extended sequential DCT
+const uint8_t SOF2 = 0xC2; // Progressive DCT
+const uint8_t SOF3 = 0xC3; // Lossless (sequential)
+
+// Start of Frame markers, differential, Huffman coding
+const uint8_t SOF5 = 0xC5; // Differential sequential DCT
+const uint8_t SOF6 = 0xC6; // Differential progressive DCT
+const uint8_t SOF7 = 0xC7; // Differential lossless (sequential)
+
+// Start of Frame markers, non-differential, arithmetic coding
+const uint8_t SOF9 = 0xC9; // Extended sequential DCT
+const uint8_t SOF10 = 0xCA; // Progressive DCT
+const uint8_t SOF11 = 0xCB; // Lossless (sequential)
+
+// Start of Frame markers, differential, arithmetic coding
+const uint8_t SOF13 = 0xCD; // Differential sequential DCT
+const uint8_t SOF14 = 0xCE; // Differential progressive DCT
+const uint8_t SOF15 = 0xCF; // Differential lossless (sequential)
+
+// Define Huffman Table(s)
+const uint8_t DHT = 0xC4;
+
+// JPEG extensions
+const uint8_t JPG = 0xC8;
+
+// Define Arithmetic Coding Conditioning(s)
+const uint8_t DAC = 0xCC;
+
+// Restart interval Markers
+const uint8_t RST0 = 0xD0;
+const uint8_t RST1 = 0xD1;
+const uint8_t RST2 = 0xD2;
+const uint8_t RST3 = 0xD3;
+const uint8_t RST4 = 0xD4;
+const uint8_t RST5 = 0xD5;
+const uint8_t RST6 = 0xD6;
+const uint8_t RST7 = 0xD7;
+
+// Other Markers
+const uint8_t SOI = 0xD8; // Start of Image
+const uint8_t EOI = 0xD9; // End of Image
+const uint8_t SOS = 0xDA; // Start of Scan
+const uint8_t DQT = 0xDB; // Define Quantization Table(s)
+const uint8_t DNL = 0xDC; // Define Number of Lines
+const uint8_t DRI = 0xDD; // Define Restart Interval
+const uint8_t DHP = 0xDE; // Define Hierarchical Progression
+const uint8_t EXP = 0xDF; // Expand Reference Component(s)
+
+// APPN Markers
+const uint8_t APP0 = 0xE0;
+const uint8_t APP1 = 0xE1;
+const uint8_t APP2 = 0xE2;
+const uint8_t APP3 = 0xE3;
+const uint8_t APP4 = 0xE4;
+const uint8_t APP5 = 0xE5;
+const uint8_t APP6 = 0xE6;
+const uint8_t APP7 = 0xE7;
+const uint8_t APP8 = 0xE8;
+const uint8_t APP9 = 0xE9;
+const uint8_t APP10 = 0xEA;
+const uint8_t APP11 = 0xEB;
+const uint8_t APP12 = 0xEC;
+const uint8_t APP13 = 0xED;
+const uint8_t APP14 = 0xEE;
+const uint8_t APP15 = 0xEF;
+
+// Misc Markers
+const uint8_t JPG0 = 0xF0;
+const uint8_t JPG1 = 0xF1;
+const uint8_t JPG2 = 0xF2;
+const uint8_t JPG3 = 0xF3;
+const uint8_t JPG4 = 0xF4;
+const uint8_t JPG5 = 0xF5;
+const uint8_t JPG6 = 0xF6;
+const uint8_t JPG7 = 0xF7;
+const uint8_t JPG8 = 0xF8;
+const uint8_t JPG9 = 0xF9;
+const uint8_t JPG10 = 0xFA;
+const uint8_t JPG11 = 0xFB;
+const uint8_t JPG12 = 0xFC;
+const uint8_t JPG13 = 0xFD;
+const uint8_t COM = 0xFE;
+const uint8_t TEM = 0x01;
+
 //extern const unsigned int IMG_data_len;
-extern std::array <uint8_t, IMG_data_len> IMG_data;
+extern std::array <uint8_t, img_data_len> img_data;
 
 #endif
